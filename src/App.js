b74492dbf3ac9607eda6dec581d96a20d6ffb771
Trevor Martin <trevor.j.martin@gmail.com>
2020-05-26 18:58:13 -0400

    add toolbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
+import ToolBar from "./components/ToolBar";
 
 const initialState = {
   squares: [],
+  clickEffect: 0,
+  generation: 0,
 };
 
 function App() {
   const [state, setState] = useState(initialState);
   const stateHooks = { get: state, set: setState };
+  const toggleMode = () => {
+    setState({ ...state, clickEffect: state.clickEffect ? 0 : 1 });
+    console.log("toggle mode");
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <Board stateHooks={stateHooks} />
+        <ToolBar
+          toggleMode={toggleMode}
+          id="toggle"
+          generation={state.generation}
+        />
+        <Board stateHooks={stateHooks} clickEffect={state.clickEffect} />
       </header>
     </div>
   );
